4ffdaf6558454c2cc4e352039521d7e31fcd4d27
mansurjon.anorboev <mansurjon.anorboev@ICT-OR-SP>
2019-07-02 15:04:35 +0500

    finished task

--- src/index.js
+++ src/index.js
@@ -3,5 +3,28 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var checked = [];
+  var result = 0;
+
+  for (let i = 1; i <= preferences.length; i++) {
+    var current = preferences[i - 1];
+
+    if(checked.includes(i))
+      continue;
+
+    if(preferences[current - 1] == i){
+      checked.push(i);
+      checked.push(current);
+      continue;
+    }
+
+    if(preferences[preferences[current - 1] - 1] == i){
+      checked.push(i);
+      checked.push(current);
+      checked.push(preferences[current - 1]);
+      result++;
+    }
+  }
+
+  return result;
 };
